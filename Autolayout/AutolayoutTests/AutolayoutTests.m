6f25e767eda27cf61d9cf4ef9eb3c4f73d7affa6
JaviInf <jinfanteizk@gmail.com>
2014-05-16 18:36:05 +0200

    ejercicio de autolayout

---
+++ Autolayout/AutolayoutTests/AutolayoutTests.m
@@ -0,0 +1,34 @@
+//
+//  AutolayoutTests.m
+//  AutolayoutTests
+//
+//  Created by iMac 9 on 04/03/14.
+//  Copyright (c) 2014 javi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutolayoutTests : XCTestCase
+
+@end
+
+@implementation AutolayoutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
