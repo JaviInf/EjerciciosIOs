4ab92cd53ff24f25aab4979f5ef9783efe09a65b
JaviInf <jinfanteizk@gmail.com>
2014-05-16 18:57:49 +0200

    Ejercicio detalles

---
+++ detalles/detalles/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  detalles
+//
+//  Created by iMac 9 on 21/02/14.
+//  Copyright (c) 2014 javi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
