7dfa31c60aabdccbafc4b58ad7fcb5601c95a8e5
JaviInf <jinfanteizk@gmail.com>
2014-05-16 19:09:21 +0200

    Ejercicio de ejemplo iconos de app

---
+++ EjemploIconos/EjemploIconos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EjemploIconos
+//
+//  Created by iMac 9 on 03/04/14.
+//  Copyright (c) 2014 javi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ EjemploIconos/EjemploIconosTests/EjemploIconosTests.m
@@ -0,0 +1,34 @@
+//
+//  EjemploIconosTests.m
+//  EjemploIconosTests
+//
+//  Created by iMac 9 on 03/04/14.
+//  Copyright (c) 2014 javi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EjemploIconosTests : XCTestCase
+
+@end
+
+@implementation EjemploIconosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
