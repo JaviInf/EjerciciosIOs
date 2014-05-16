2d513abed2bb156012fb3605c24853dbbd08ef69
JaviInf <jinfanteizk@gmail.com>
2014-05-16 18:55:22 +0200

    Ejercicio galeria de ftos optimizada para ipad

---
+++ GaleriaFotosIpad/GaleriaFotosTests/GaleriaFotosTests.m
@@ -0,0 +1,34 @@
+//
+//  GaleriaFotosTests.m
+//  GaleriaFotosTests
+//
+//  Created by iMac 9 on 03/03/14.
+//  Copyright (c) 2014 javi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GaleriaFotosTests : XCTestCase
+
+@end
+
+@implementation GaleriaFotosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
